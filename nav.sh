b49056a313a97cc54fc644e522c4883b04232307
kesry <2011939212@qq.com>
2023-07-26 11:16:16 +0800

    v1.1 nav.sh修正

--- nav.sh
+++ nav.sh
@@ -2,7 +2,7 @@
 
 VERSION=v1.1
 wget https://ghproxy.com/https://raw.githubusercontent.com/kesry/nav/v1.1/Dockerfile \
-&& sudo docker build -t nav:$VERSION .\
-&& sudo docker run -itd -p 9000:8080 -v ./nav/db:/nav/db -v ./nav/log:/nav/log  --name=nav nav:$VERSION
+&& sudo docker build -t nav:$VERSION .
+#&& sudo docker run -itd -p 9000:8080 -v ./nav/db:/nav/db -v ./nav/log:/nav/log  --name=nav nav:$VERSION
 
 exit 0
